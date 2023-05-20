782941d30872c7c3f8bef67dd0ade8c2b118ca35
Josh Taylor <jtaylor4@bootcampspot.com>
2023-05-20 16:02:35 -0700

    implement graphql client and refactor home page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_TECH = gql`
   query tech {
@@ -10,8 +10,20 @@ export const QUERY_TECH = gql`
 `;
 
 export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
+  query matchups {
+    matchups {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
+
+export const QUERY_MATCHUP = gql`
+  query matchup($_id: String) {
+    matchup(_id: $_id) {
       _id
       tech1
       tech2
